8554968f29a8f82e4b509c236d9f160b291bf070
Marco Castañeda <marco_pascual410@hotmail.com>
2024-08-03 22:52:17 -0600

    Changed db

--- keystone.ts
+++ keystone.ts
@@ -2,14 +2,12 @@ import "./env";
 import lists from "./models/schema";
 import { config } from "@keystone-6/core";
 import { withAuth, session } from "./auth/auth";
-import { KeystoneContext } from "@keystone-6/core/types";
-import { Role } from "./models/Role/constants";
 
 export default withAuth(
   config({
     db: {
-      provider: "postgresql",
-      url: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.POSTGRES_DB}?connect_timeout=300`,
+      provider: "mysql",
+      url: `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.MYSQL_DB}?connect_timeout=300`,
     },
     server: {
       cors: true,
