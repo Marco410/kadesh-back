b9efabe83ab43d9e29b0759986f9537bbd85cf60
Marco Castañeda <marco_pascual410@hotmail.com>
2025-12-05 12:10:26 -0600

    fix: envs

--- env.ts
+++ env.ts
@@ -2,4 +2,7 @@
 const path = require("path");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: path.resolve(process.cwd(), "config", ".env.dev") });
+
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config({ path: path.resolve(process.cwd(), "config", ".env.dev") });
+  }
