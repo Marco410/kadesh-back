ddea9a26f1e0c58ecd2afb26b239e31218b53e6e
Marco Castañeda <marco_pascual410@hotmail.com>
2025-12-09 15:43:30 -0600

    feat: db seed

--- utils/seed/index.ts
+++ utils/seed/index.ts
@@ -1,13 +1,13 @@
 import { KeystoneContext } from "@keystone-6/core/types";
 import { createAnimalTypes } from "./animal_types";
 import { createUserAdmin } from "./user";
-import { createVeterinaryService } from "./veterinary_services";
 import { createVeterinary } from "./veterinary";
+import { createPetPlaceService } from "./veterinary_services";
 
 export default async function seed(context: KeystoneContext) {
   await createAnimalTypes(context);
   const userID = await createUserAdmin(context);
-  const services = await createVeterinaryService(context);
+  const services = await createPetPlaceService(context);
 
   await createVeterinary(context, userID, services);
 }
