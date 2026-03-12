c0cbf135648f1703545224cbafd906e2fe6ef14c
Marco Castañeda <marco_pascual410@hotmail.com>
2026-03-12 13:57:47 -0600

    seed roles

--- utils/seed/index.ts
+++ utils/seed/index.ts
@@ -1,4 +1,5 @@
 import { KeystoneContext } from "@keystone-6/core/types";
+import { ROLES } from "../../models/Role/constants";
 import { createAnimalTypes } from "./animal_types";
 import { createUserAdmin } from "./user";
 import { createVeterinary } from "./veterinary";
@@ -7,7 +8,24 @@ import { createPetPlaceService } from "./veterinary_services";
 import { createSaasCompany } from "./saas_company";
 import { createSaasPlan } from "./saas_plan";
 
+async function createRoles(context: KeystoneContext) {
+  for (const role of ROLES) {
+    const existing = await context.db.Role.findOne({
+      where: { name: role.value },
+    });
+
+    if (!existing) {
+      await context.db.Role.createOne({
+        data: {
+          name: role.value,
+        },
+      });
+    }
+  }
+}
+
 export default async function seed(context: KeystoneContext) {
+  await createRoles(context);
   await createAnimalTypes(context);
   await createPetPlaceTypes(context);
   const userID = await createUserAdmin(context);
